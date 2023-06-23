4a194c9da8fc0a7acda75e3e0234f014eca3d4d5
muriel-guedes-work <murielguedes@proton.me>
2023-06-23 17:20:14 -0300

    .
    .

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,81 @@
-mod buffer;
+use std::{io::{Write, Read}, process::{Stdio, Child}, path::Path};
+use serde_json::{json, Value};
+use lsp_types::*;
+
 mod utils;
-mod style;
-mod settings;
-mod run;
 
-use std::time::Duration;
-
-use style::{Style, Color};
-use settings::{Settings, LeftBarStyle};
+pub struct Server {
+    server: Child
+}
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            server: std::process::Command::new("rust-analyzer")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn().unwrap()
+        }
+    }
+    pub fn write(&mut self, v: Value) {
+        let parsed = serde_json::to_string(&v).unwrap();
+        println!("sending: {parsed}");
+        self.server.stdin.as_mut().unwrap().write_all(
+            format!("Content-Length: {}\r\n\r\n{parsed}", parsed.len()).as_bytes()
+        ).unwrap();
+    }
+    pub fn read(&mut self) {
+        loop {
+            let mut buf = String::new();
+            self.server.stdout.as_mut().unwrap().read_to_string(&mut buf).unwrap();
+            if buf.len() > 0 {
+                println!("received: {buf}")
+            }
+        }
+    }
+    pub fn init(&mut self, root: &Path) {
+        self.write(json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "initialize",
+            "params": json!(InitializeParams {
+                process_id: Some(1),
+                client_info: Some(ClientInfo {
+                    name: "doro".to_string(),
+                    version: Some("3.17.0".to_string())
+                }),
+                root_uri: Some(Url::from_directory_path(root).unwrap()),
+                trace: Some(TraceValue::Verbose),
+                ..Default::default()
+            })
+        }));
+        self.write(json!({
+            "method": "initialized"
+        }));
+    }
+    pub fn didopen(&mut self, path: &Path) {
+        let text = std::fs::read_to_string(path).unwrap();
+        self.write(json!({
+            "method": "textDocument/didOpen",
+            "parms": DidOpenTextDocumentParams {
+                text_document: TextDocumentItem {
+                    uri: Url::from_file_path(path).unwrap(),
+                    language_id: utils::get_file_language_id(path),
+                    text,
+                    version: 1
+                }
+            }
+        }));
+    }
+}
+impl Drop for Server {
+    fn drop(&mut self) {
+        self.server.wait().unwrap();
+    }
+}
 
 fn main() {
-    utils::init();
-    run::run(Settings {
-        update_time: Duration::from_secs_f32(1. / 60.),
-        tab: "    ",
-        left_bar: LeftBarStyle {
-            style: Style::new(Color::Black, Color::BrightBlack),
-            divider: " "
-        },
-        text: Style::new(Color::Black, Color::White)
-    });
-    utils::exit();
+    let mut server = Server::new();
+    server.init(Path::new("C://Users/User09/Documents/GitHub/doro"));
+    server.didopen(Path::new("C://Users/User09/Documents/GitHub/doro/src/main.rs"));
+    server.read()
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,49 +1,8 @@
-use std::{io::{stdout, Write}, panic};
-use crossterm::{execute, terminal::{self, ClearType}, cursor};
+use std::path::Path;
 
-pub fn clear_screen() {
-    execute!(stdout(), cursor::Hide).unwrap();
-    for y in 0..terminal::size().unwrap().1 {
-        execute!(stdout(), cursor::MoveTo(0, y)).unwrap();
-        execute!(stdout(), terminal::Clear(ClearType::CurrentLine)).unwrap();
-    }
-    execute!(stdout(), cursor::MoveTo(0, 0)).unwrap();
-    stdout().flush().unwrap();
-}
-pub fn clear_screen_all() {
-    execute!(
-        stdout(),
-        cursor::MoveTo(0, 0),
-        terminal::Clear(ClearType::All),
-        cursor::Hide
-    ).unwrap();
-    stdout().flush().unwrap();
-}
-pub fn show_cursor(x: u16, y: u16) {
-    execute!(stdout(), cursor::MoveTo(x, y), cursor::Show).unwrap()
-}
-
-pub fn init() {
-	panic::set_hook(Box::new(|panic_info| {
-        exit();
-        std::fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .write(true)
-            .open("./doro_logs.log").unwrap()
-            .write_all(panic_info.to_string().as_bytes()).unwrap();
-        eprintln!("{panic_info}")
-	}));
-    terminal::enable_raw_mode().unwrap();
-    execute!(stdout(),
-        terminal::SetTitle("DORO"),
-        terminal::DisableLineWrap,
-        cursor::SetCursorStyle::SteadyBar
-    ).unwrap();
-    clear_screen_all();
-}
-
-pub fn exit() {
-    clear_screen_all();
-    terminal::disable_raw_mode().unwrap();
+pub fn get_file_language_id(path: &Path) -> String {
+    match path.extension().unwrap().to_str().unwrap() {
+        "rs" => "rust",
+        ext => panic!("Extension: '{ext}', not supported !")
+    }.to_string()
 }
