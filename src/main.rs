c88cba143cda892dc3424a3c661b099e5333ce03
muriel-guedes-work <murielguedes@proton.me>
2023-06-30 16:17:48 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,73 @@
-use std::{io::{Write, Read}, process::{Stdio, Child}, path::Path};
-use serde_json::{json, Value};
-use lsp_types::*;
-
-mod utils;
-
-pub struct Server {
-    server: Child
-}
-impl Server {
-    pub fn new() -> Self {
-        Self {
-            server: std::process::Command::new("rust-analyzer")
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn().unwrap()
-        }
-    }
-    pub fn write(&mut self, v: Value) {
-        let parsed = serde_json::to_string(&v).unwrap();
-        println!("sending: {parsed}");
-        self.server.stdin.as_mut().unwrap().write_all(
-            format!("Content-Length: {}\r\n\r\n{parsed}", parsed.len()).as_bytes()
-        ).unwrap();
-    }
-    pub fn read(&mut self) {
-        loop {
-            let mut buf = String::new();
-            self.server.stdout.as_mut().unwrap().read_to_string(&mut buf).unwrap();
-            if buf.len() > 0 {
-                println!("received: {buf}")
-            }
-        }
-    }
-    pub fn init(&mut self, root: &Path) {
-        self.write(json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "initialize",
-            "params": json!(InitializeParams {
-                process_id: Some(1),
-                client_info: Some(ClientInfo {
-                    name: "doro".to_string(),
-                    version: Some("3.17.0".to_string())
-                }),
-                root_uri: Some(Url::from_directory_path(root).unwrap()),
-                trace: Some(TraceValue::Verbose),
-                ..Default::default()
-            })
-        }));
-        self.write(json!({
-            "method": "initialized"
-        }));
-    }
-    pub fn didopen(&mut self, path: &Path) {
-        let text = std::fs::read_to_string(path).unwrap();
-        self.write(json!({
-            "method": "textDocument/didOpen",
-            "parms": DidOpenTextDocumentParams {
-                text_document: TextDocumentItem {
-                    uri: Url::from_file_path(path).unwrap(),
-                    language_id: utils::get_file_language_id(path),
-                    text,
-                    version: 1
-                }
-            }
-        }));
-    }
-}
-impl Drop for Server {
-    fn drop(&mut self) {
-        self.server.wait().unwrap();
-    }
-}
+use home::{cargo_home, rustup_home};
+use std::{env, path::Path};
+use ra_ap_cfg::CfgDiff;
+use ra_ap_ide::{FilePosition, TextSize};
+use ra_ap_project_model::{
+    CargoConfig, CargoWorkspace, CfgOverrides, ManifestPath, ProjectWorkspace, Sysroot,
+    WorkspaceBuildScripts,
+};
+use ra_ap_rust_analyzer::cli::load_cargo;
+use ra_ap_vfs::{AbsPathBuf, VfsPath};
 
 fn main() {
-    let mut server = Server::new();
-    server.init(Path::new("C://Users/User09/Documents/GitHub/doro"));
-    server.didopen(Path::new("C://Users/User09/Documents/GitHub/doro/src/main.rs"));
-    server.read()
+    let mainfest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let manifest_dir = Path::new(&mainfest_dir);
+
+    let cargo_toml = ManifestPath::try_from(AbsPathBuf::assert(
+        manifest_dir.join("Cargo.toml").to_path_buf()
+    )).unwrap();
+
+    let extra_env = ra_ap_ide_db::FxHashMap::default();
+
+    fn do_nothing(_s: String) {}
+
+    let cargo = CargoWorkspace::new(
+        CargoWorkspace::fetch_metadata(
+            &cargo_toml,
+            &AbsPathBuf::assert(manifest_dir.to_path_buf()),
+            &CargoConfig::default(),
+            &do_nothing
+        ).unwrap()
+    );
+
+    do_nothing(String::default());
+
+    let toolchain = format!("stable-{}", rustc_host::from_cli().unwrap());
+
+    let ws = ProjectWorkspace::Cargo {
+        cargo,
+        build_scripts: WorkspaceBuildScripts::default(),
+        sysroot: Sysroot::load(
+            AbsPathBuf::assert(rustup_home().unwrap().join("toolchains").join(toolchain)),
+            AbsPathBuf::assert(cargo_home().unwrap().join("registry").join("src")),
+        )
+        .ok(),
+        rustc: None,
+        rustc_cfg: vec![],
+        cfg_overrides: CfgOverrides::Wildcard(CfgDiff::new(vec![], vec![]).unwrap()),
+        toolchain: None,
+        target_layout: None
+    };
+
+    let load_config = load_cargo::LoadCargoConfig {
+        load_out_dirs_from_check: true,
+        with_proc_macro: false,
+        prefill_caches: false
+    };
+    let (analysis_host, vfs, _proc_macro_server) = load_cargo::load_workspace(ws, &extra_env, &load_config).unwrap();
+
+    let vfs_path = VfsPath::new_real_path(
+        manifest_dir
+            .join("src")
+            .join("main.rs")
+            .to_string_lossy()
+            .to_string()
+    );
+    let file_id = vfs.file_id(&vfs_path).unwrap();
+    let offset = TextSize::try_from(1070usize).unwrap(); // refer to line 58, `do_nothing( String::default() );`
+    let file_position = FilePosition { file_id, offset };
+
+    let analysis = analysis_host.analysis();
+    let definition = analysis.goto_definition(file_position).unwrap();
+    println!("{:?}", definition);
 }
--- src/utils.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-pub fn get_file_language_id(path: &Path) -> String {
-    match path.extension().unwrap().to_str().unwrap() {
-        "rs" => "rust",
-        ext => panic!("Extension: '{ext}', not supported !")
-    }.to_string()
-}
